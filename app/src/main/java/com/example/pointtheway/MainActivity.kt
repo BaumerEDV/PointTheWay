62398560916f6c7919829096ce4de2f4fe3722b7
Stefan Erwin Baumer <baumeredv@gmail.com>
2021-05-15 11:50:57 +0200

    added a button that will later allow to choose the target location

--- app/src/main/java/com/example/pointtheway/MainActivity.kt
+++ app/src/main/java/com/example/pointtheway/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.pointtheway
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.motion.widget.Debug
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectTargetLocationButton: Button = findViewById(R.id.selectTargetLocationButton)
+        selectTargetLocationButton.setOnClickListener {
+            selectTargetLocation()
+        }
+
+
+
     }
+
+    private fun selectTargetLocation() {
+        Log.d("Stefan", "selectTargetLocation() executed")
+    }
+
+
 }
