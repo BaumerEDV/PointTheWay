d9d85832c23f387d231781596df85808e9a55577
Stefan Erwin Baumer <baumeredv@gmail.com>
2021-05-16 15:36:49 +0200

    can now move the map marker around and set it for the target location

--- app/src/main/java/com/example/pointtheway/SelectLocationActivity.kt
+++ app/src/main/java/com/example/pointtheway/SelectLocationActivity.kt
@@ -1,29 +1,28 @@
 package com.example.pointtheway
 
-import android.app.Activity
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
 import android.util.Log
-import android.view.View
-import androidx.fragment.app.FragmentManager
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapFragment
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
 class SelectLocationActivity : AppCompatActivity() {
+    var selectedLocationMarker: Marker? = null;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_location)
 
-        val fragment = supportFragmentManager.findFragmentById(R.id.shop_fragment)
-        if (fragment !is SupportMapFragment){
-            Log.e("Stefan", "The Map is a " + fragment.toString())
+        val googleMapsFragment = supportFragmentManager.findFragmentById(R.id.shop_fragment)
+        if (googleMapsFragment !is SupportMapFragment){
+            Log.e("Stefan", "The Map is a " + googleMapsFragment.toString())
             throw AssertionError("The Map is not what it's supposed to be!")
         }
-        fragment.getMapAsync{
+        googleMapsFragment.getMapAsync{
             onMapReady(it)
         }
 
@@ -32,10 +31,20 @@ class SelectLocationActivity : AppCompatActivity() {
 
     private fun onMapReady(googleMap: GoogleMap) {
         Log.d("Stefan", "Map is ready!")
-        googleMap.setOnMapClickListener { onMapClick(it) }
+        googleMap.setOnMapClickListener { onMapClick(it, googleMap) }
     }
 
-    private fun onMapClick(point: LatLng){
+    private fun onMapClick(point: LatLng, googleMap: GoogleMap){
         Log.d("Stefan", "Latitude: ${point.latitude}; Longitude: ${point.longitude}")
+
+        if(selectedLocationMarker == null){
+            selectedLocationMarker = googleMap.addMarker(MarkerOptions()
+                .position(point)
+                .title("Target Location")
+                .draggable(true)
+            )
+        } else {
+            selectedLocationMarker?.position = point
+        }
     }
 }
