323ca5921ad285867ec6ef0655f50eb36c00452f
Stefan Erwin Baumer <baumeredv@gmail.com>
2021-05-17 13:26:19 +0200

    now have a working, pointing arrow needs refactoring

--- app/src/main/java/com/example/pointtheway/PointToLocationActivity.kt
+++ app/src/main/java/com/example/pointtheway/PointToLocationActivity.kt
@@ -1,20 +1,202 @@
 package com.example.pointtheway
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.model.LatLng
 
-class PointToLocationActivity : AppCompatActivity() {
+class PointToLocationActivity : AppCompatActivity(), LocationListener,
+    SensorEventListener {
+
+    private lateinit var targetPosition: LatLng
+    private lateinit var locationManager: LocationManager
+    private lateinit var sensorManager: SensorManager
+    private lateinit var debugDisplay: TextView
+    private lateinit var compassNeedle: ImageView
+
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+    private var angleRelativeToMagneticNorth: Double = 0.0
+
+    private var headingAngle: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_point_to_location)
 
-        val targetPosition : LatLng? = intent?.extras?.getParcelable(SELECTED_POSITION_KEY)
-        Log.d("Stefan", "targetPosition is $targetPosition")
+        debugDisplay = findViewById(R.id.debugDisplay)
 
-        Toast.makeText(this, targetPosition?.toString(), Toast.LENGTH_LONG).show()
+        val selectLocationButton: Button = findViewById(R.id.selectTargetLocationButton)
+        compassNeedle = findViewById(R.id.compassNeedle)
+
+        selectLocationButton.setOnClickListener{
+            val switchActivityIntent = Intent(this, SelectLocationActivity::class.java)
+            startActivity(switchActivityIntent)
+        }
+
+        val position: LatLng? = intent?.extras?.getParcelable(SELECTED_POSITION_KEY)
+        if (position == null) {
+            val switchActivityIntent = Intent(this, SelectLocationActivity::class.java)
+            startActivity(switchActivityIntent)
+        } else {
+            targetPosition = position
+            Log.d("Stefan", "targetPosition is $targetPosition")
+        }
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, this)
+        } else {
+            Log.d("Stefan", "no location permission, third activity is swapping to first")
+            val switchActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(switchActivityIntent)
+        }
+
+        Log.d("Stefan", "location manager instantiated ($locationManager), starting sensor manager now...")
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        Log.d("Stefan", "sensor manager instantiated ($sensorManager)")
+
+        Toast.makeText(this, targetPosition.toString(), Toast.LENGTH_LONG).show()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                this,
+                magneticField,
+                SensorManager.SENSOR_DELAY_NORMAL,
+                SensorManager.SENSOR_DELAY_UI
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+            Log.d("StefanSensor", "Accelerometer update")
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+            Log.d("StefanSensor", "magnetic field update")
+        }
+        updateOrientationAngles()
+    }
+
+    fun updateOrientationAngles() {
+        // Update rotation matrix, which is needed to update orientation angles.
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+
+        // "rotationMatrix" now has up-to-date information.
+
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+        // "orientationAngles" now has up-to-date information.
+
+        val angleRelativeToMagneticNorth = -orientationAngles[0] * 360 / (2 * 3.14159f) //West: 90, East -90, South +-179
+        this.angleRelativeToMagneticNorth = turnAzimuthRotationInto360DegreeFormat(
+            angleRelativeToMagneticNorth.toDouble()
+        )
+
+
+
+        //Toast.makeText(this, "Azimuth-Rotation: $rotation", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Azimuth: ${orientationAngles[0]}, Pitch: ${orientationAngles[1]}, Roll: ${orientationAngles[2]}", Toast.LENGTH_SHORT).show()
+    }
+
+
+    override fun onLocationChanged(location: Location) {
+        val currentPoint = LatLng(location.latitude, location.longitude)
+        headingAngle = angleFromCoordinate(currentPoint, targetPosition) //West: 90, East: 270
+
+        updateHeadingUI()
+    }
+
+    fun updateHeadingUI(){
+        debugDisplay.text = "Azimuth-Rotation: $angleRelativeToMagneticNorth;\n Heading: $headingAngle"
+        compassNeedle.rotation = (angleRelativeToMagneticNorth - headingAngle).toFloat()
+    }
+
+    fun turnAzimuthRotationInto360DegreeFormat(rotation: Double): Double{
+        if(rotation > 0){
+            return rotation
+        } else {
+            val rotationAbs = Math.abs(rotation)
+            return 180 + (180 - rotationAbs)
+        }
+    }
+
+    fun angleFromCoordinate(point1: LatLng, point2: LatLng): Double{
+        return angleFromCoordinate(point1.latitude, point1.longitude, point2.latitude, point2.longitude)
+    }
+
+    fun angleFromCoordinate(lat1: Double, long1: Double, lat2: Double, long2: Double): Double{
+        val dLon = (long2 - long1)
+
+        val y = Math.sin(dLon) * Math.cos(lat2)
+        val x = Math.cos(lat1) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(dLon)
+
+        var brng = Math.atan2(y, x)
+
+        //brng = Math.degrees(brng)
+        brng = Math.toDegrees(brng)
+        brng = (brng + 360) % 360
+        brng = 360 - brng // count degrees clockwise - remove to make counter-clockwise
+
+        return brng
+    }
+
+    override fun onProviderEnabled(provider: String) {
+    }
+
+    override fun onProviderDisabled(provider: String) {
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
     }
 }
--- app/src/main/java/com/example/pointtheway/SelectLocationActivity.kt
+++ app/src/main/java/com/example/pointtheway/SelectLocationActivity.kt
@@ -1,21 +1,35 @@
 package com.example.pointtheway
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 const val SELECTED_POSITION_KEY = "com.example.pointtheway.POSITION_KEY"
 
-class SelectLocationActivity : AppCompatActivity() {
-    var selectedLocationMarker: Marker? = null;
-    var selectLocationButton: Button? = null;
+class SelectLocationActivity : AppCompatActivity(), LocationListener {
+    var selectedLocationMarker: Marker? = null
+    var selectLocationButton: Button? = null
+    val FINE_LOCATION_PERMISSION_REQUEST_CODE = 1
+    var googleMap: GoogleMap? = null
+    var isMapZoomedInAlready: Boolean = false
+    var locationManager: LocationManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,26 +37,25 @@ class SelectLocationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_select_location)
 
         val googleMapsFragment = supportFragmentManager.findFragmentById(R.id.shop_fragment)
-        if (googleMapsFragment !is SupportMapFragment){
+        if (googleMapsFragment !is SupportMapFragment) {
             Log.e("Stefan", "The Map is a " + googleMapsFragment.toString())
             throw AssertionError("The Map is not what it's supposed to be!")
         }
-        googleMapsFragment.getMapAsync{
+        googleMapsFragment.getMapAsync {
             onMapReady(it)
         }
 
-        val cancelButton : Button = findViewById(R.id.cancelLocationButton)
-        cancelButton.setOnClickListener{
+        val cancelButton: Button = findViewById(R.id.cancelLocationButton)
+        cancelButton.setOnClickListener {
             val switchActivityIntent = Intent(this, MainActivity::class.java)
             //TODO: make this swap back to the previous activity instead
             startActivity(switchActivityIntent)
         }
 
         selectLocationButton = findViewById(R.id.confirmLocationButton)
-        selectLocationButton?.setOnClickListener{
+        selectLocationButton?.setOnClickListener {
             val switchActivityIntent = Intent(this, PointToLocationActivity::class.java).apply {
                 putExtra(SELECTED_POSITION_KEY, selectedLocationMarker?.position)
-                //Log.d("Stefan", "put extra ${selectedLocationMarker?.position}")
             }
             startActivity(switchActivityIntent)
         }
@@ -51,22 +64,98 @@ class SelectLocationActivity : AppCompatActivity() {
     }
 
     private fun onMapReady(googleMap: GoogleMap) {
-        Log.d("Stefan", "Map is ready!")
+        this.googleMap = googleMap
         googleMap.setOnMapClickListener { onMapClick(it, googleMap) }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            googleMap.isMyLocationEnabled = true
+
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, this)
+        } else {
+            Log.d("Stefan", "dont have location permission")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                FINE_LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+
+
+        Log.d("Stefan", "Map is ready!")
     }
 
-    private fun onMapClick(point: LatLng, googleMap: GoogleMap){
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isEmpty()) {
+            //permissions denied
+            val switchActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(switchActivityIntent)
+            return
+        }
+        when (requestCode) {
+            FINE_LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    googleMap?.isMyLocationEnabled = true
+
+                    locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    locationManager?.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, this)
+                } else {
+                    return
+                }
+            }
+        }
+    }
+
+
+    private fun onMapClick(point: LatLng, googleMap: GoogleMap) {
         Log.d("Stefan", "Latitude: ${point.latitude}; Longitude: ${point.longitude}")
 
-        if(selectedLocationMarker == null){
-            selectedLocationMarker = googleMap.addMarker(MarkerOptions()
-                .position(point)
-                .title("Target Location")
-                .draggable(true)
+        if (selectedLocationMarker == null) {
+            selectedLocationMarker = googleMap.addMarker(
+                MarkerOptions()
+                    .position(point)
+                    .title("Target Location")
+                    .draggable(true)
             )
         } else {
             selectedLocationMarker?.position = point
         }
         selectLocationButton?.isEnabled = true
     }
+
+    override fun onLocationChanged(location: Location) {
+        if (isMapZoomedInAlready) {
+            return
+        }
+        val cameraPosition =
+            CameraPosition.Builder().target(LatLng(location.latitude, location.longitude))
+                .zoom(17.0.toFloat()).build()
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        googleMap?.moveCamera(cameraUpdate)
+        isMapZoomedInAlready = true
+        locationManager?.removeUpdates(this)
+    }
+
+    override fun onProviderEnabled(provider: String) {
+    }
+
+    override fun onProviderDisabled(provider: String) {
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
 }
